e27e9b1f2f9542722732688c4703e188fd0476be
Aditya Shendkar <aditya.shendkar03@gmail.com>
2024-03-13 22:21:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+
+function addPlayer(event) {
+    event.preventDefault();
+
+    const playerData = {
+        name: `${document.getElementById("first-name").value} ${document.getElementById("last-name").value
+            }`,
+        country: document.getElementById("country").value,
+        score: parseInt(document.getElementById("score").value),
+    };
+
+    playersList.push(playerData);
+    updatePlayerList();
+    clear();
+}
+
+
+
+function updatePlayerList() {
+    const playerListElement = document.getElementById("player-list");
+
+    playersList.sort((a, b) => b.score - a.score);
+
+    playerListElement.innerHTML = "";
+
+    playersList.forEach((player, index) => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `
+      <p>${player.name}</p>
+      <p>${player.country}</p>
+      <p>${player.score}</p>
+      
+      <button onclick="increaseScoreHandler(${index})">+5</button>
+      <button onclick="deleteScoreHandler(${index})"><i class="fa-solid fa-trash"></i></button>
+      <button onclick="decreaseScoreHandler(${index})">-5</button>`;
+
+        playerListElement.appendChild(listItem);
+    });
+}
+
+function clear() {
+    document.getElementById("first-name").value = "";
+    document.getElementById("last-name").value = "";
+    document.getElementById("country").value = "";
+    document.getElementById("score").value = "";
+}
+function deleteScoreHandler(index) {
+    playersList.splice(index, 1);
+    updatePlayerList();
+}
+
+function increaseScoreHandler(index) {
+    playersList[index].score += 5;
+    updatePlayerList();
+}
+
+function decreaseScoreHandler(index) {
+    if (playersList[index].score <= 0) {
+        playersList[index].score = 0;
+        alert("Action Not Possible");
+        // return;
+    } else {
+        playersList[index].score -= 5;
+        updatePlayerList();
+    }
+}
+
+
+const playersList = [
+    {
+        name: "Rohit Sharma",
+        country: "India",
+        score: 80,
+    },
+    {
+        name: "Virat Kohli",
+        country: "India",
+        score: 90,
+    },
+    {
+        name: "MS Dhoni",
+        country: "India",
+        score: 100,
+    },
+];
+
+window.onload = () => updatePlayerList();
